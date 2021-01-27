cf74cc09904441760b037e0f2211d914db9b2972
hayit08 <72305423+hayit08@users.noreply.github.com>
2021-01-27 12:28:39 +0500

    refactor:change style

--- src/index.js
+++ src/index.js
@@ -1,24 +1,18 @@
 
-exports.min = function min (array) {
+exports.min = function min(array) {
   if (array == undefined) return 0;
-  if (array.length > 0) return Math.min(...array) 
-  else return 0;
-      }
-
-
-
-
-
-
-
-exports.max = function max (array) {
-
-  if (array == undefined) return 0;
-  if (array.length > 0) return Math.max(...array) 
+  if (array.length > 0) return Math.min(...array)
   else return 0;
 }
 
-exports.avg = function avg (array) {
+exports.max = function max(array) {
+
+  if (array == undefined) return 0;
+  if (array.length > 0) return Math.max(...array)
+  else return 0;
+}
+
+exports.avg = function avg(array) {
 
   if (array == undefined) return 0;
   if (array.length > 0) return array.reduce((acc, item) => acc + item) / array.length;
