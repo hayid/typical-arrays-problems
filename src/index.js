cabca1bac2815794bee07a40f8a13dcc231e55d0
hayit08 <72305423+hayit08@users.noreply.github.com>
2021-01-27 12:22:48 +0500

    feat: test completed

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 
 exports.min = function min (array) {
-  return 0;
-}
+  if (array == undefined) return 0;
+  if (array.length > 0) return Math.min(...array) 
+  else return 0;
+      }
+
+
+
+
+
+
 
 exports.max = function max (array) {
-  return 0;
+
+  if (array == undefined) return 0;
+  if (array.length > 0) return Math.max(...array) 
+  else return 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+
+  if (array == undefined) return 0;
+  if (array.length > 0) return array.reduce((acc, item) => acc + item) / array.length;
+  else return 0;
 }
